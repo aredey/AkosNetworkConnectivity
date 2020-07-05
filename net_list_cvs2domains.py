1c0b23d56c3fbbdac9a8aa22c8bd2aacb217a67e
Akos Redey <github151225@redey.net>
2020-07-05 21:26:39 +0100

    First (development) version of net_list_cvs2domains.py added, a few config and supplementary files changed

---
+++ net_list_cvs2domains.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+'''
+This scripts reads in a network (security domains - IP networks) list 
+file and and IP address (on the command line) and outputs the associated
+security domain
+
+Location: https://github.com/aredey/AkosNetworkConnectivity
+version: 0.1          author: Akos Redey               date: 2020.07.xx
+'''
+import pprint
+import argparse
+import csv
+import netaddr  #sudo pip3 install netaddr
+
+
+########################################################################
+#Command line info gathering: 
+config_directory = 'Configs/'                 #default for *.cfg
+network_list_csv = 'Configs/network-list.csv' #default is network-list.csv
+
+parser = argparse.ArgumentParser(description='Mapping IP address(es) to \
+        Networks/Security Domains')
+parser.add_argument('ip_addr', type=str, help='Source or "the" IP address')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet',   action='store_true', help='Display minimal information')
+group.add_argument('-v', '--verbose', action='store_true', help='Chatty information display')
+parser.add_argument('-d','--dst_addr', type=str, metavar='', help='Destination IP address')
+#parser.add_argument('-c','--cfg_dir',type=str, metavar='', help='Directory containg *.cfg files')
+parser.add_argument('-n','--net_list',type=str, metavar='', help='Network list .csv file')
+args = parser.parse_args()
+
+source = args.ip_addr
+destination = args.dst_addr
+#if args.cfg_dir:
+#  config_directory = args.cfg_dir
+if args.net_list:
+  network_list_csv = args.net_list
+
+if args.quiet:
+  print(source)
+  print(destination)
+#  print(config_directory)
+  print(network_list_csv)
+elif args.verbose:
+  print("[Source] IP:                           {}".format(source))
+  print("Destination IP:                        {}".format(destination))
+  print("Location of *.cfg configuration files: {}".format(config_directory))
+  print("Network definition csv file:           {}".format(network_list_csv))
+#else:                                     #default command lin display
+#  print("source:           {}".format(source))
+#  print("destination:      {}".format(destination))
+#  print("config_directory: {}".format(config_directory))
+#  print("network_list_csv: {}".format(network_list_csv))
+
+########################################################################
+#Importing the .csv network definition file and get the IP address from 
+#the command line and print out the longest match network and associated
+#domain where the IP address belongs
+domains = ({})
+with open(network_list_csv, 'r') as csvfile:
+  records = csv.DictReader(csvfile)
+  for row in records:
+    dom = row['Network']
+    net_address = row['NetworkAddress']+"/"+row['PrefixLength']
+    try:
+      domains[dom]
+    except KeyError:
+      domains[dom] = set()
+    domains[dom].add(net_address)
+
+if args.verbose:
+  print("### Domains and constituent networks:")
+  for doms, nets in domains.items():
+    #print(doms, nets)
+    print(doms,":")
+    for n in nets:
+      print("\t",n)
+  
+
+########################################################################
+# Display network address and domain for matches
+def match_network(ip_address):
+  for sec_domain, nets in domains.items():
+    for ip_network in nets:
+      #print(sec_domain, ip_network, ip_address)
+      if netaddr.IPAddress(ip_address) in netaddr.IPNetwork(ip_network):
+        print('"{}" is member of "{}" that is part of the "{}" domain'.format(ip_address, ip_network, sec_domain))
+
+if destination:
+  print("The Source:")
+  match_network(source)
+  print("\nThe Destination:")
+  match_network(destination)
+else:
+  print("The IP address:")
+  match_network(source)
+  
